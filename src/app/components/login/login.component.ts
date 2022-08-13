ea5c304a04d87c21fcfa981cb951ab30223a1d22
Felipe-Euzebio <felipe.euzebio.ti@gmail.com>
2022-08-13 17:37:03 -0300

    Autenticação de LOGIN

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Credenciais } from 'src/app/models/credenciais';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -15,25 +17,36 @@ export class LoginComponent implements OnInit {
     senha: ''
   }
 
-  email = new FormControl(null, [Validators.required, Validators.email]);
-  senha = new FormControl(null, [Validators.required, Validators.minLength(3)]);
+  email = new FormControl(null, [Validators.required, Validators.email])
+  senha = new FormControl(null, [Validators.required, Validators.minLength(3)])
 
-  constructor(private toast: ToastrService) { }
+  constructor(
+    private toast: ToastrService,
+    private service: AuthService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
+
   validaCampos(): boolean {
-    if (this.email.valid && this.senha.valid) {
-      return true;
-    } else {
-      return false;
-    }
+    return this.email.valid && this.senha.valid
   }
 
+
   logar() {
-    this.toast.error('Usuário e/ou senha inválidos!', 'Login');
-    this.creds.senha = '';
+
+    this.service.authenticate(this.creds).subscribe(resposta => {
+
+      this.service.successFullLogin(resposta.headers.get('Authorization').substring(7))
+      this.router.navigate([''])
+
+    }, () => {
+
+      this.toast.error('Usuário e/ou senha inválidos!')
+
+    })
+
   }
 
 }
