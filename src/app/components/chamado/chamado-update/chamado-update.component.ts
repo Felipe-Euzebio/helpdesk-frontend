21d02f8d1b521ce7ba3ed981b2df4b69a937be29
Felipe-Euzebio <felipe.euzebio.ti@gmail.com>
2022-08-22 20:52:34 -0300

    Serviço para componente CHAMADO-UPDATE implementado

--- src/app/components/chamado/chamado-update/chamado-update.component.ts
+++ src/app/components/chamado/chamado-update/chamado-update.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { Chamado } from 'src/app/models/chamado';
 import { Cliente } from 'src/app/models/cliente';
@@ -24,7 +24,7 @@ export class ChamadoUpdateComponent implements OnInit {
     tecnico: '',
     cliente: '',
     nomeCliente: '',
-    nomeTecnico: ''
+    nomeTecnico: '',
   }
 
   clientes: Cliente[] = [];
@@ -42,12 +42,24 @@ export class ChamadoUpdateComponent implements OnInit {
     private clienteService: ClienteService,
     private tecnicoService: TecnicoService,
     private toastService: ToastrService,
-    private router: Router
+    private router: Router,
+    private activatedRoute: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
+    this.chamado.id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.findById();
     this.findAllClientes();
     this.findAllTecnicos();
+  }
+
+
+  findById(): void{
+    this.chamadoService.findById(this.chamado.id).subscribe(resposta => {
+      this.chamado = resposta;
+    }, ex => {
+      this.toastService.error(ex.error.error);
+    });
   }
 
 
@@ -65,11 +77,11 @@ export class ChamadoUpdateComponent implements OnInit {
   }
 
 
-  create(): void {
+  update(): void {
 
-    this.chamadoService.create(this.chamado).subscribe(() => {
+    this.chamadoService.update(this.chamado).subscribe(() => {
 
-      this.toastService.success('Chamado criado com sucesso!', 'Novo Chamado');
+      this.toastService.success('Chamado atualizado com sucesso!', 'Atualizar Chamado');
       this.router.navigate(['chamados']);
 
     }, ex => {
@@ -84,6 +96,28 @@ export class ChamadoUpdateComponent implements OnInit {
   validaCampos(): boolean {
     return this.prioridade.valid && this.status.valid && this.titulo.valid
       && this.observacoes.valid && this.tecnico.valid && this.cliente.valid;
+  }
+
+
+  retornaStatus(status: any): string {
+    if(status == '0') {
+      return 'ABERTO'
+    } else if(status == '1') {
+      return 'EM ANDAMENTO'
+    } else {
+      return 'ENCERRADO'
+    }
+  }
+
+  
+  retornaPrioridade(prioridade: any): string {
+    if(prioridade == '0') {
+      return 'BAIXA'
+    } else if(prioridade == '1') {
+      return 'MÉDIA'
+    } else {
+      return 'ALTA'
+    }
   }
 
 }
