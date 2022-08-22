e42e33ebcf34b8f0f4f778b2e51fe7315aa03fe9
Felipe-Euzebio <felipe.euzebio.ti@gmail.com>
2022-08-22 19:49:20 -0300

    Serviço para CHAMADO-CREATE. Ajustes no componente CLIENTE-LIST

--- src/app/components/cliente/cliente-list/cliente-list.component.ts
+++ src/app/components/cliente/cliente-list/cliente-list.component.ts
@@ -13,7 +13,7 @@ export class ClienteListComponent implements OnInit {
 
   ELEMENT_DATA: Cliente[] = [];
 
-  displayedColumns: string[] = ['id', 'titulo', 'cliente', 'dataAbertura', 'prioridade', 'status', 'acoes'];
+  displayedColumns: string[] = ['id', 'nome', 'cpf', 'email', 'acoes'];
   dataSource = new MatTableDataSource<Cliente>(this.ELEMENT_DATA);
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
